9ed3a3709aa99af2198f6f0a7a4fff2cbb568b43
salvoguastella <salvo.guastella90@gmail.com>
2022-03-04 17:13:48 +0000

    cleanup

--- src/components/FooterBody/partials/NavSectionList/index.js
+++ src/components/FooterBody/partials/NavSectionList/index.js
@@ -2,19 +2,9 @@ import styled from "styled-components";
 import Component from "./NavSectionList";
 
 const NavSectionList = styled(Component)`
-  display: grid;
-  width: 71.5vw;
-  grid-auto-columns: 1fr;
-  grid-column-gap: 3.55vw;
-  grid-row-gap: 16px;
-  -ms-grid-columns: 1fr;
-  grid-template-columns: 1fr;
-  -ms-grid-rows: auto;
-  grid-template-rows: auto;
-
   @media ${({ theme }) => theme.device.mdUp} {
+    display: grid;
     width: 57vw;
-    grid-auto-columns: 1fr;
     grid-column-gap: 7.1vw;
     grid-row-gap: 7.1vw;
     -ms-grid-columns: 1fr 1fr;
@@ -33,13 +23,10 @@ const NavSectionList = styled(Component)`
 
   @media ${({ theme }) => theme.device.lgUp} {
     width: 71.5vw;
-    grid-auto-columns: 1fr;
     grid-column-gap: 3.55vw;
     grid-row-gap: 16px;
     -ms-grid-columns: 1fr 1fr 1fr 1fr;
     grid-template-columns: 1fr 1fr 1fr 1fr;
-    -ms-grid-rows: auto;
-    grid-template-rows: auto;
     .displayText {
       padding: 0;
       h6 {
