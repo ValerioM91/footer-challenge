24bf902e516c27acd08f604d77ae386b7a22c800
Valerio Mattera <matteravalerio@gmail.com>
2022-03-04 13:14:13 +0000

    global theme

---
+++ src/theme/colors.js
@@ -0,0 +1,7 @@
+const colors = {
+  white: "#ffffff",
+  footerBackgroundColor: "#1A212A",
+  primaryGreen: "#CBFF00",
+};
+
+export default colors;
---
+++ src/theme/typography.js
@@ -0,0 +1,14 @@
+const fontSizes = {
+  mobile: {
+    small: "1.1rem",
+    medium: "1.3rem",
+    large: "1.5rem",
+  },
+  tablet: {
+    small: "1.1rem",
+    medium: "1.5rem",
+    large: "1.8rem",
+  },
+};
+
+export default fontSizes;
