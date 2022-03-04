85e7ee75aa3feba581309caa821b62f235dddcfd
salvoguastella <salvo.guastella90@gmail.com>
2022-03-04 17:11:26 +0000

    responsiveness

--- src/App.js
+++ src/App.js
@@ -10,7 +10,11 @@ export default () => {
   return (
     <ThemeProvider theme={theme}>
       <Container>
-        <section>Sample content</section>
+        <section
+          style={{ padding: "5rem", fontSize: "2rem", textAlign: "center" }}
+        >
+          Sample content
+        </section>
       </Container>
       <Footer />
     </ThemeProvider>
--- src/components/FooterBody/index.js
+++ src/components/FooterBody/index.js
@@ -6,9 +6,18 @@ const FooterBody = styled(Component)`
   flex-direction: column;
   justify-content: space-between;
   align-items: flex-start;
+  padding: 6rem 0 9.5rem;
+  .logo {
+    align-self: center;
+    margin-bottom: 3.5rem;
+  }
   @media ${({ theme }) => theme.device.mdUp} {
-    padding: 6rem 0 5rem;
+    padding: 6rem 0 14rem;
     flex-direction: row;
+    .logo {
+      align-self: flex-start;
+      margin-bottom: 0;
+    }
   }
   @media ${({ theme }) => theme.device.lgUp} {
     padding: 14rem 0;
